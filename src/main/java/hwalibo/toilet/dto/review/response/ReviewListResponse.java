71ee499505dcfe2a0f609f59b5ecab25b2ac8839
최성민 <smsm7288@gmail.com>
2025-10-31 16:17:26 +0900

    주석 ì처리

--- src/main/java/hwalibo/toilet/dto/review/response/ReviewListResponse.java
+++ src/main/java/hwalibo/toilet/dto/review/response/ReviewListResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
  * 특정 화장실의 리뷰 목록 조회 API의 최종 data 필드를 구성하는 DTO입니다.
  * 리뷰 목록과 함께 추후 페이징 정보, 총 개수 등의 확장을 고려한 Wrapper 클래스입니다.
  */
-@Getter
+/*@Getter
 public class ReviewListResponse {
 
     private final List<ReviewResponse> reviews;
@@ -16,4 +16,4 @@ public class ReviewListResponse {
     public ReviewListResponse(List<ReviewResponse> reviews) {
         this.reviews = reviews;
     }
-}
+}*/
