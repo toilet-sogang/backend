9e15eb17fa2df462f08c3d542e38163036269116
donghoong <slimgreg5208@gmail.com>
2025-11-11 21:19:09 +0900

    리뷰 업로드 수정

--- src/main/java/hwalibo/toilet/service/user/UserService.java
+++ src/main/java/hwalibo/toilet/service/user/UserService.java
@@ -24,8 +24,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
