243db7d9ec78463570f92e5a05b33a78c42b2676
donghoong <slimgreg5208@gmail.com>
2025-11-23 11:44:15 +0900

    시간 문제 해결

--- src/main/java/hwalibo/toilet/ToiletApplication.java
+++ src/main/java/hwalibo/toilet/ToiletApplication.java
@@ -1,16 +1,24 @@
 package hwalibo.toilet;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.TimeZone;
+
 @EnableAsync
 @EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class ToiletApplication {
+	@PostConstruct
+	public void started() {
+		// 시간을 한국 시간(KST)으로 고정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToiletApplication.class, args);
