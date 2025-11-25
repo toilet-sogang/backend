fa0c7c92351c58a7b12beba5aca71036836b3a00
최성민 <smsm7288@gmail.com>
2025-11-26 00:33:27 +0900

    frontend ã새로운 배포 주소 작성

--- src/main/java/hwalibo/toilet/config/web/WebConfig.java
+++ src/main/java/hwalibo/toilet/config/web/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://frontendproj-three.vercel.app",
+                        "https://hwalibo.vercel.app",
                         "https://hwalibo-backend.duckdns.org",
                         "http://localhost:5173"
                 )
